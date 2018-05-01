c55fe77e77ce6e9ef3d565aece2ad09b48cd2850
demirev <demirev15@gmail.com>
2018-05-01 17:30:35 +0200

    policy interpolation

--- R/functions/dynprog.R
+++ R/functions/dynprog.R
@@ -1,3 +1,6 @@
+library(splines)
+library(gam)
+
 discretize_m <- function(
   P_bounds = c(0.5,1.5),
   p_bounds = c(0.5,1.5),
@@ -31,7 +34,7 @@ discretize_k <- function(
   return(seq(SS_k_low, SS_k_high, length.out = num_out))
 }
 
-fit_policy <- function(V) {
+fit_loess <- function(V) {
   if (length(unique(V$k)) > 1) {
     fit <- loess(
       action ~ m + k, 
@@ -45,6 +48,53 @@ fit_policy <- function(V) {
   }
   
   pred_func <- function(m,k){
+    if (length(m) > 1 & length(k) == 1) {
+      k <- rep(k, length(m))
+    }
+    predict(fit, data.frame(m, k, value = 0, action = 0))
+  }
+  return(pred_func)
+}
+
+fit_spline <- function(V, df = 3) {
+  if (length(unique(V$k)) > 1) {
+    fit <- gam(
+      action ~ s(m, df = df) + s(k, df = df), 
+      data = V
+    )
+  } else {
+    fit <- gam(
+      action ~  s(m, df = 10), 
+      data = V
+    )
+  }
+  
+  pred_func <- function(m,k){
+    if (length(m) > 1 & length(k) == 1) {
+      k <- rep(k, length(m))
+    }
+    predict(fit, data.frame(m, k, value = 0, action = 0))
+  }
+  return(pred_func)
+}
+
+fit_loess2 <- function(V, degree = 1) {
+  if (length(unique(V$k)) > 1) {
+    fit <- gam(
+      action ~ lo(m, degree = degree) + lo(k, degree = degree), 
+      data = V
+    )
+  } else {
+    fit <- gam(
+      action ~ lo(m, degree = 2), 
+      data = V
+    )
+  }
+  
+  pred_func <- function(m,k){
+    if (length(m) > 1 & length(k) == 1) {
+      k <- rep(k, length(m))
+    }
     predict(fit, data.frame(m, k, value = 0, action = 0))
   }
   return(pred_func)
@@ -61,21 +111,43 @@ opt_pol <- function(
   k_law = function(k) {return(k)},
   prodf = Cobb_Douglas$new(alpha = 0.3),
   utilf = Iso_Elastic$new(rho = 1),
-  cgrid = 40
+  psi = LogNormalShock$new(),
+  xi  = LogNormalShock$new(),
+  cgrid = 40,
+  ndraw = 10
 ) {
   
   #polf  <- fit_policy(V)
   #browser()
+  # action-space grid
   c_seq <- seq(0, m, length.out = cgrid) # cannot consume more than m
   
+  # law of motion
   future_k <- k_law(k)
   r <- prodf$MPKk(future_k)
   
+  # stohastic grid
+  psi_r <- psi$draw(ndraw)
+  colnames(psi_r) <- c("psi","psi_p")
+  xi_r  <- xi$draw(ndraw)
+  colnames(xi_r)  <- c("xi", "xi_p")
+  stoh_grid <- psi_r %>%
+    crossing(xi_r)
+  stoh_grid$prob <- stoh_grid$psi_p * stoh_grid$xi_p
+  
   foc <- function(c) {
-    future_m <- (1 - delta + r)*(m - c)/(1 - D) #/psi + xi in expectation dissapear
+    # future assets for all psi-xi realizations
+    future_m <- (1 - delta + r)*(m - c)/((1 - D)*stoh_grid$psi) + stoh_grid$xi
+    future_m[future_m < 0] <- 0 # negative assets don't make sense
+    # query policy at given future value
     future_c <- polf(future_m, future_k)
-    future_c[future_c == 0] <- 1e-16 # small but positive
+    # take expectation
+    future_c <- sum(future_c * stoh_grid$prob)
+    # for numerical stability
+    future_c[future_c < 0] <- 1e-16 # small but positive
+    # expected marginal utility
     exp_mu <- utilf$MU(future_c)
+    # Euler equation
     return(utilf$MU(c) - beta*(1 - delta + r)*exp_mu)
   }
   
@@ -92,8 +164,9 @@ opt_pol <- function(
 
 pf_iter <- function(
   # iteration parameters
-  tol = 1e-3, 
+  tol = 2e-3, 
   maxiter = 50,
+  fit_policy = fit_spline,
   verbose = T,
   # model parameters
   alpha = 0.36,
@@ -104,12 +177,17 @@ pf_iter <- function(
   k_law = function(k) {return(k)},
   prodf = Cobb_Douglas$new(alpha = 0.36),
   utilf = Iso_Elastic$new(rho = 1),
+  psi = NoShock$new(const = 1),
+  xi  = NoShock$new(const = 0),
+  #psi = LogNormalShock$new(),
+  #xi  = NormalShock$new(),
   # optimal action choice parameters
   cgrid = 40,
+  ndraw = 500,
   # discretization parameters
   P_bounds_m = c(0.5, 1.5),
   p_bounds_m = c(0.5, 1.5),
-  num_out_m = 200,
+  num_out_m = 80,
   above_m = 1.2,
   P_bounds_k = c(1,1),
   below_k = 1,
@@ -169,7 +247,10 @@ pf_iter <- function(
           k_law = k_law,
           prodf = prodf,
           utilf = utilf,
-          cgrid = cgrid
+          psi = psi, 
+          xi = xi,
+          cgrid = cgrid,
+          ndraw = ndraw
         ) # calculate new optimal action, given current policy at next state
         V$action[i] <<- opt 
         return(opt)
@@ -178,23 +259,78 @@ pf_iter <- function(
     # check convergence
     diff <- max(abs(V$action - oldact))
     iter <- iter + 1
-    if (verbose) cat("Completed iteration", iter, ". Diff:", diff, "\n")
+    if (verbose) cat("Completed iteration", iter, "Diff:", diff, "\n")
   }
   
   return(list(VTable = V, policy = fit_policy(V)))
 }
 
 
-# V_finalP <- pf_iter(
-#   thetas = c(0.75,1.25), 
-#   delta = 0.3, 
-#   beta = 0.9, 
-#   alpha = 0.75, 
-#   rho = 2, 
-#   tr_matrix = matrix(rep(0.5,4), nrow = 2, ncol = 2), 
-#   prodf = prodf, 
-#   utilf = utilf, 
-#   numk = 80,
-#   tol = 1e-3,
-#   maxiter = 50
+policy_plot <- function(
+  policy, 
+  state_space,
+  main = "Policy Plot",
+  ylb = "c/m",
+  xlb = "m"
+) {
+  
+  state_space$action <- policy(state_space$m, state_space$k)
+  
+  if (length(unique(state_space$k)) == 1) {
+    p <- ggplot(data = state_space, aes(x = m, y = action/m)) +
+      geom_path() +
+      theme_bw() + 
+      ggtitle(main) +
+      xlab(xlb) + 
+      ylab(ylb)
+  } else {
+    stop("You haven't written this yet")
+  }
+  p 
+}
+
+# test1 <- pf_iter() # default settings - no shocks
+# 
+# test2 <- pf_iter(
+#   xi = NormalShock$new(sigma = 0.01), 
+#   psi = LogNormalShock$new(sigma = 0.01), 
+#   ndraw = 200
 # )
+# test2_2 <- pf_iter(
+#   xi = NormalShock$new(sigma = 0.1), 
+#   psi = LogNormalShock$new(sigma = 0.1), 
+#   ndraw = 200
+# )
+# 
+# test3 <- pf_iter(
+#   xi = NormalShock$new(sigma = 0.01), 
+#   #psi = LogNormalShock$new(sigma = 0.01), 
+#   ndraw = 500
+# )
+# test3_2 <- pf_iter(
+#   xi = NormalShock$new(sigma = 0.01), 
+#   #psi = LogNormalShock$new(sigma = 0.01), 
+#   ndraw = 1500
+# )
+# test3_3 <- pf_iter(
+#   xi = NormalShock$new(sigma = 0.1), 
+#   #psi = LogNormalShock$new(sigma = 0.01), 
+#   ndraw = 500
+# )
+# 
+# test4 <- pf_iter(
+#   #xi = NormalShock$new(sigma = 0.01), 
+#   psi = LogNormalShock$new(sigma = 0.01), 
+#   ndraw = 500
+# )
+# test4_2 <- pf_iter(
+#   #xi = NormalShock$new(sigma = 0.01), 
+#   psi = LogNormalShock$new(sigma = 0.01), 
+#   ndraw = 1500
+# )
+# test4_3 <- pf_iter(
+#   #xi = NormalShock$new(sigma = 0.01), 
+#   psi = LogNormalShock$new(sigma = 0.1), 
+#   ndraw = 500
+# )
+
