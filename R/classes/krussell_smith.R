4d7be85a1816f228b1ee8e49fb0437272a9b6c81
demirev <demirev15@gmail.com>
2018-05-06 14:57:45 +0200

    stohastic optimization method started

--- R/classes/krussell_smith.R
+++ R/classes/krussell_smith.R
@@ -70,12 +70,12 @@ KS_Economy <- R6Class(
       return(cumWealth / sum(allWealth))
     },
     
-    step = function(verbose = T, predraw_xi = 30, predraw_psi = 30) {
+    step = function(verbose = T, fixK = FALSE) {
       # simulate a single period
       #browser()
       # 1. Produce
-      self$P  <- self$P * self$PSI$draw(1)$value # aggregate permanent shock
-      trShock <- self$XI$draw(1)$value # aggreagete transitory shock
+      self$P  <- self$P * self$PSI$sample(1) # aggregate permanent shock
+      trShock <- self$XI$sample(1) # aggreagete transitory shock
       # factor prices
       self$W = self$FF$MPL(
         K = self$K, 
@@ -104,7 +104,7 @@ KS_Economy <- R6Class(
       # 2. Invest and choose labor
       
       # next period aggregate capital is the sum of all investments
-      self$K <- self$Agents %>%
+      investments <- self$Agents %>%
         lapply(
           function(agent) {
             agent$invest(k = self$K / self$nagents)
@@ -112,7 +112,8 @@ KS_Economy <- R6Class(
         ) %>%
         reduce(c) %>%
         sum
-      
+      if (!fixK) self$K <- investments
+       
       # agents can potentially also change their income
       laborDecision <- self$Agents %>%
         lapply(
@@ -147,13 +148,15 @@ KS_Economy <- R6Class(
       return(T)
     },
     
-    wealth_ss = function(tol = 0.0002, maxit = 6000, verbose = T) {
+    wealth_ss = function(tol = 0.02, maxit = 6000, minit = 500, verbose = T) {
+      # simulates the economy until a steady wealth distribution is reached
+      
       wealthdist <- self$quantileSummary() # initialize wealth distr
       diff <- 2*tol # initialize higher than tol
       it <- 0
-      while (diff >= tol & it <= maxit) {
+      while ((diff >= tol & it <= maxit) | (it <= minit)) {
         # simulate 1 period
-        S <- self$step(verbose = F)
+        S <- self$step(verbose = F, fixK = T)
         # calculate wealth distribution
         newdist <- self$quantileSummary()
         # track change
@@ -163,10 +166,69 @@ KS_Economy <- R6Class(
         if (verbose & it %% 100 == 0) {
           cat("Current Difference:", round(diff,4), "\n")
           print(wealthdist)
+          KoL <- self$K / self$L
+          cat("Current K/L:", KoL, "\n")
         }
         it <- it + 1
       }
-      return(TRUE)
+      return(self$K / self$L)
+    },
+    
+    stohastic_optimization = function(
+      k = 38.9, 
+      tol = 0.02, 
+      lrate = .3, 
+      verbose = T
+    ) {
+      # iterates between policy estimation and simulation until the K/L implied
+      # by the policy matches the one used to estimate it
+      converged <- F
+      
+      while (!converged) {
+        # 1. Calculate Policy Given steady state guess
+        update_policies <- self$Agents %>%
+          lapply(
+            function(agent) {
+              agent$updatePolicy(
+                alpha = self$FF$alpha,
+                delta = self$delta, 
+                prodf = self$FF, 
+                ss_k = k,
+                start_action = agent$QTable$action # hot start value iteration
+              )
+            }
+          )
+        # 2. Simulate
+        wealth_ss <- self$wealth_ss(verbose = T)
+        new_k <- self$Agents %>%
+          lapply(
+            
+            function(agent) {
+              agent$wallets$M
+            }
+          ) %>%
+          reduce(c) %>%
+          sum # implied K by agents policy
+        new_k <- new_k / self$L
+        diff <- abs(new_k - k)
+        if (diff < tol) {
+          converged <- TRUE
+        } else {
+          if (new_k > 3*k) {
+            k <- 3*k # avoid enourmous updates
+          } else {
+            k <- (1 - lrate) * k + lrate * new_k
+          }
+        }
+        
+        self$K <- k * self$L
+        
+        # 3. Message
+        if (verbose) {
+          cat("Iteration complete. Diff:", diff, "k's:", new_k, k, "\n")
+        }
+      }
+      
     }
   )
 )
@@ -176,7 +238,9 @@ AgentType <- R6Class(
   public = list(
     psi = LogNormalShock$new(sigma = 0.025), # productivity permanent shock
     xi = EmploymentShock$new(sigma = 0.04), # productivity transitory shock
+    utilf = Iso_Elastic$new(rho = 1), # utility function
     policy = NULL, #function(m,k) return(0.5*m), # consumption policy
+    QTable = data.frame(m = 0, k = 0, action = 0), # optimal action table
     D = 0.00625, # probability of death
     beta = 0.985, # discount factor
     n = 1000, # number of agents for simulation
@@ -189,7 +253,9 @@ AgentType <- R6Class(
       L = 1, # units of labor available
       psi = LogNormalShock$new(sigma = 0.025), # productivity permanent shock
       xi = EmploymentShock$new(sigma = 0.04), # productivity transitory shock
+      utilf = Iso_Elastic$new(rho = 1), # utility function
       pol = function(m,k) return(0.5*m),
+      QTable = data.frame(m = 0, k = 0, action = 0),
       D = 0.00625, # probability of death
       beta = 0.985, # discount factor
       n = 1000
@@ -197,7 +263,9 @@ AgentType <- R6Class(
       # assign fields
       self$psi <- psi
       self$xi  <- xi
+      self$utilf <- utilf
       self$policy <- pol
+      self$QTable <- QTable
       self$D <- D
       self$beta <- beta
       self$n
@@ -234,6 +302,7 @@ AgentType <- R6Class(
     },
     
     die = function() {
+      # kill off (and respawn) some of the agents
       dead <- (runif(self$n) < self$D)
       bequest <- self$wallets$M * dead
       self$wallets$M[dead] <- 0
@@ -247,6 +316,43 @@ AgentType <- R6Class(
       # increase resources by inheritence
       self$wallets$M <- self$wallets$M + bequest
       return(T)
+    },
+    
+    updatePolicy = function(alpha, delta, prodf, ss_k, start_action = NULL) {
+      # given parameters of the economy solves for optimal policy
+      # this version assumes no aggregate shocks, so only parameter is 
+      # steady state capital
+      
+      newPol <- pf_iter(
+        # iteration parameters
+        tol = 3e-2, 
+        maxiter = 50,
+        fit_policy = fit_spline,
+        verbose = T,
+        # model parameters
+        alpha = alpha,
+        beta  = self$beta,
+        delta = delta,
+        D = self$D,
+        # model functions
+        k_law = function(k) {return(k)}, # always in steady state
+        prodf = prodf,
+        utilf = self$utilf,
+        psi = self$psi,
+        xi  = self$xi,
+        # optimal action choice parameters
+        cgrid = 40,
+        ndraw = 20,
+        # discretization parameters
+        m_seq = discretize_m(
+          max_m = 35,
+          num_out = 130
+        ),
+        k_seq = ss_k # steady state capital
+      )
+      
+      self$policy <- newPol$policy
+      self$QTable <- newPol$QTable
     }
   )
 )
@@ -254,22 +360,26 @@ AgentType <- R6Class(
 testKs <- KS_Economy$new(
   Agents = list(
     AgentType$new(
-      M = 10.7, 
+      M = 38.9, 
       p = 1, 
       l = 1/0.9,
       L = 1,
       psi = LogNormalShock$new(sigma = 0.025), 
       xi = EmploymentShock$new(sigma = 0.04), 
+      utilf = Iso_Elastic$new(rho = 1),
       pol = test6_3$policy,
+      QTable = test6_3$VTable,
       D = 0.00625,
       beta = 0.985,
       n = 10000
     )
   ),
-  K = 10.7 * 1000, 
+  K = 38.9 * 10000, 
   P = 1, 
   FF = Cobb_Douglas$new(alpha = 0.36),
   PSI = NoShock$new(mu = 1),
   XI  = NoShock$new(mu = 1),
   delta = 0.025 
 )
+
+testKs$stohastic_optimization()
