5885bce97ee2c5a85702d461fbbc1a41c92bc50e
Christoph <christoph.semken@barcelonagse.eu>
2018-05-25 19:38:57 +0200

    first joined histogram

--- R/04_mpc_analysis.R
+++ R/04_mpc_analysis.R
@@ -15,98 +15,126 @@ parameters <- list(
          liq = c(beta_mid=0.9671509686, beta_range=0.04406199825),
          liq_off = c(beta_mid=0.953226385, beta_range=0.04243624967))
 )
+variables = c("liq", "liq_off") # wealth variables to look at
 
 
 # solve on final parameters -----------------------------------------------
 
-# contains one model for each country and model type, e.g. models$AT$betas_liq
-models = list()
-mpcs = list() # contains list of every agent’s MPC, e.g. models$AT
-
-country = "AT" # TODO put this in a loop
-
-# everythin below this lines should go in a country loop 
-models[[country]] = list()
-
-for (specification in c("liq", "liq_off")) { 
-  estimated_betas = parameters[[country]][[specification]]
-  beta_min = estimated_betas["beta_mid"] - estimated_betas["beta_range"]
-  beta_max = estimated_betas["beta_mid"] + estimated_betas["beta_range"]
-  beta_n = 7 # we only consider the model with seven agent types
-  
-  # verify that estimated betas make sense
-  #if (beta_min <= 0) stop("lowest beta cannot be smaller than 0")
-  #if (beta_max >= 1) stop("highest beta cannot be greater than 1")
-  
-  betas <- seq(
-    from = max(beta_min, 0),
-    to = min(beta_max, 1), 
-    length.out = beta_n
-  )
-  
-  models[[country]][[specification]] <- KS_Economy$new(
-    Agents = lapply(
-      betas,
-      function(beta) {
-        AgentType$new(
-          p = 1, # personal productivity initial
-          l = 1/0.9, # fraction of labor supplied
-          L = 1, # units of labor available
-          psi = LogNormalShock$new(
-            mu = 0, 
-            sigma = get(country, parameters)$sigma_psi
-          ), # productivity permanent shock
-          xi = EmploymentShock$new(
-            mu = 0, 
-            sigma = get(country, parameters)$sigma_xi,
-            mu_ins = 0.15, # percent insurance
-            OM = 0.07 # probability of unemployment
-          ), # productivity transitory shock
-          utilf = Iso_Elastic$new(rho = 1), # utility function
-          pol = function(m,k) return(0.5*m),
-          D = 0.00625, # probability of death
-          n = 3000, # number to simulate
-          max_m = 35,
-          num_out = 40,
-          beta = beta # discount factor
-        )
-      }
-    ), # list of agents
-    P = 1, # aggregate productivity multiplier
-    FF = Cobb_Douglas$new(alpha = 0.36), # production function
-    PSI = NoShock$new(mu = 1), # aggregate permanent productivity schock
-    XI  = NoShock$new(mu = 1), # aggregate transitory productivity schock
-    delta = 0.025, # depreciation
-    Z = 1 # TFP
-  )
-  
-  # Solve the model
-  models[[country]][[specification]]$optimizeStohastic(
-    k = 38.9, # initial value for capital
-    tol = .02, # tolerance for the update of k_law
-    tol_policy = .01, # tolerance for individual agent policies
-    tol_ss = .02, # tolerance for steady state wealth distribution convergence
-    lrate = .3, # speed of update for k_law
-    max_m = 35, # discretization of m-space (highest gird point)
-    num_out = 45, # discretization of m-space (number of grid points)
-    fit_policy = fit_spline, # interpolation funciton
-    verbose = 1, # print detailed messages or not
-    probs = seq(0,1,0.1) # output percentiles - make sure they match Target
-  )
+getFinalModels <- function(country_params) {
+  models = list()
+  for (wealth_var in variables) { 
+    estimated_betas = country_params[[wealth_var]]
+    beta_min = estimated_betas["beta_mid"] - estimated_betas["beta_range"]
+    beta_max = estimated_betas["beta_mid"] + estimated_betas["beta_range"]
+    beta_n = 7 # we only consider the model with seven agent types
+    
+    # verify that estimated betas make sense
+    #if (beta_min <= 0) stop("lowest beta cannot be smaller than 0")
+    #if (beta_max >= 1) stop("highest beta cannot be greater than 1")
+    
+    betas <- seq(
+      from = max(beta_min, 0),
+      to = min(beta_max, 1), 
+      length.out = beta_n
+    )
+    
+    models[[wealth_var]] <- KS_Economy$new(
+      Agents = lapply(
+        betas,
+        function(beta) {
+          AgentType$new(
+            p = 1, # personal productivity initial
+            l = 1/0.9, # fraction of labor supplied
+            L = 1, # units of labor available
+            psi = LogNormalShock$new(
+              mu = 0, 
+              sigma = country_params$sigma_psi
+            ), # productivity permanent shock
+            xi = EmploymentShock$new(
+              mu = 0, 
+              sigma = country_params$sigma_xi,
+              mu_ins = 0.15, # percent insurance
+              OM = 0.07 # probability of unemployment
+            ), # productivity transitory shock
+            utilf = Iso_Elastic$new(rho = 1), # utility function
+            pol = function(m,k) return(0.5*m),
+            D = 0.00625, # probability of death
+            n = 3000, # number to simulate
+            max_m = 35,
+            num_out = 40,
+            beta = beta # discount factor
+          )
+        }
+      ), # list of agents
+      P = 1, # aggregate productivity multiplier
+      FF = Cobb_Douglas$new(alpha = 0.36), # production function
+      PSI = NoShock$new(mu = 1), # aggregate permanent productivity schock
+      XI  = NoShock$new(mu = 1), # aggregate transitory productivity schock
+      delta = 0.025, # depreciation
+      Z = 1 # TFP
+    )
+    
+    # Solve the model
+    models[[wealth_var]]$optimizeStohastic(
+      k = 38.9, # initial value for capital
+      tol = .02, # tolerance for the update of k_law
+      tol_policy = .01, # tolerance for individual agent policies
+      tol_ss = .02, # tolerance for steady state wealth distribution convergence
+      lrate = .3, # speed of update for k_law
+      max_m = 35, # discretization of m-space (highest gird point)
+      num_out = 45, # discretization of m-space (number of grid points)
+      fit_policy = fit_spline, # interpolation funciton
+      verbose = 1, # print detailed messages or not
+      probs = seq(0,1,0.1) # output percentiles - make sure they match Target
+    )
+  }
 }
 
-mpcs[[country]] = list()
-for (specification in c("liq", "liq_off")) {
-  all_mpcs = unlist(models[[country]][[specification]]$calcMPC()[["mpc_list"]])
-  mpcs[[country]][[specification]] = all_mpcs
-  
-  # histogram
-  hist(all_mpcs, main = paste("Histogram of", country, specification), freq = F)
-  
-  # histogram overlapping
-  
-  # summary stats: mean, median, standard deviation
-  
-  # box plots, stripplot (python in R), v plots
+getMpcs <- function(models) {
+  "Returns a list of lists with the mpc of every individual in each calibrated model"
+  lapply(models, function(model){
+    unlist(model$calcMPC()[["mpc_list"]])
+  })
 }
+
+plotMpcHist <- function(mpcs_list, country_name="NA", alpha=0.25) {
+  "Plots a joint histogram of mpcs for multiple models"
+  first <- T
+  n <- length(mpcs_list)
+  colors <- palette(rainbow(n, alpha=alpha)) 
+  figs = lapply(mpcs_list, hist)
+  for (i in seq(n)) {
+    var_name <- names(mpcs_list)[[i]]
+    plot(figs[[i]], col=colors[[i]], add=!first) 
+    first = F
+  }
+}
+
+models = sapply(parameters, getFinalModels)
+mpcs = lapply(models, getMpcs)
+
+sapply(mpcs, plotMpcHist)
+
+
+# country = "AT" # TODO put this in sapply
+# # contains one model for each country and model type, e.g. models$AT$betas_liq
+# models = list()
+# mpcs = list() # contains list of every agent’s MPC, e.g. models$AT
+# 
+# models[[country]] = getFinalModels()
+# 
+# mpcs[[country]] = list()
+# for (wealth_var in c("liq", "liq_off")) {
+#   all_mpcs = unlist(models[[country]][[wealth_var]]$calcMPC()[["mpc_list"]])
+#   mpcs[[country]][[wealth_var]] = all_mpcs
+#   
+#   # histogram
+#   hist(all_mpcs, main = paste("Histogram of", country, wealth_var), freq = F)
+#   
+#   # histogram overlapping
+#   
+#   # summary stats: mean, median, standard deviation
+#   
+#   # box plots, stripplot (python in R), v plots
+# }
 
